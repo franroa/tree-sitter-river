b73c3483073c53d26dc47446e98989cd93131f01
Francisco Roa <froa@tecnosylva.com>
2025-06-03 11:45:59 +0200

    fix: new grammar

---
+++ bindings/swift/TreeSitterRiver/river.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_RIVER_H_
+#define TREE_SITTER_RIVER_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_river(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_RIVER_H_
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/franroa/tree-sitter-river.git
+
+go 1.22
+
+require github.com/tree-sitter/go-tree-sitter v0.24.0
